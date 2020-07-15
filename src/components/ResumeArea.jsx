899c80a60456db20bed93162a2bcf4006003f43b
Baraka Larbi <larbibaraka@gmail.com>
2020-07-15 20:07:27 +0100

    adding stats area

--- src/components/ResumeArea.jsx
+++ src/components/ResumeArea.jsx
@@ -1,70 +1,76 @@
-import React from 'react'
+import React from "react";
 import profileImage from "../assets/images/profile.jpg";
-export  const  ResumeArea = () => {
-    return (
-     
-        <div className="flex flex-row resume-info justify-around">
-        <div className="h-40 w-1/5 border rounded-lg overflow-hidden shadow-lg bg-white  m-10 px-4 py-2 flex justify-center">
-          <div className="flex items-center">
-            <img
-              className="w-16 h-16 rounded-full mr-4"
-              src={profileImage}
-              alt=""
-            />
-            <div className="text-lg">
-              <p className="text-gray-800 leading-none font-bold">
-                Jonathan Reinink
-              </p>
-              <p className="text-gray-500">Total Patients</p>
+export const ResumeArea = () => {
+  return (
+    <div className="flex flex-row resume-info">
+      <div className="flex flex-row justify-between">
+        <div className="h-full w-10/12 flex flex-row justify-between m-5">
+          <div className="h-40  border rounded-lg overflow-hidden shadow-lg bg-white  flex justify-center">
+            <div className="flex items-center p-3">
+              <img
+                className="w-16 h-16 rounded-full mr-4"
+                src={profileImage}
+                alt=""
+              />
+              <div className="text-lg">
+                <p className="text-gray-800 leading-none font-bold">
+                  Jonathan Reinink
+                </p>
+                <p className="text-gray-500">Total Patients</p>
+              </div>
+            </div>
+          </div>
+          <div className="h-40  border rounded-lg overflow-hidden shadow-lg bg-white  flex justify-center">
+            <div className="flex items-center p-3">
+              <img
+                className="w-16 h-16 rounded-full mr-4"
+                src={profileImage}
+                alt=""
+              />
+              <div className="text-lg">
+                <p className="text-gray-800 leading-none font-bold">
+                  Jonathan Reinink
+                </p>
+                <p className="text-gray-500">Available Staff</p>
+              </div>
+            </div>
+          </div>
+          <div className="h-40  border rounded-lg overflow-hidden shadow-lg bg-white  flex justify-center">
+            <div className="flex items-center p-3">
+              <img
+                className="w-16 h-16 rounded-full mr-4"
+                src={profileImage}
+                alt=""
+              />
+              <div className="text-lg">
+                <p className="text-gray-800 leading-none font-bold">
+                  Jonathan Reinink
+                </p>
+                <p className="text-gray-500">Avg Treat. Costs</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="h-40 w-1/5 border rounded-lg overflow-hidden shadow-lg bg-white  m-10 px-4 py-2 flex justify-center">
-          <div className="flex items-center">
-            <img
-              className="w-16 h-16 rounded-full mr-4"
-              src={profileImage}
-              alt=""
-            />
-            <div className="text-lg">
-              <p className="text-gray-800 leading-none font-bold">
-                Jonathan Reinink
-              </p>
-              <p className="text-gray-500">Available Staff</p>
+        <div className="h-full w-2/12 flex m-5">
+          
+            <div className="h-40  border rounded-lg overflow-hidden shadow-lg bg-white  p-2 flex justify-center">
+              <div className="flex items-center p-5">
+                <img
+                  className="w-16 h-16 rounded-full mr-4"
+                  src={profileImage}
+                  alt=""
+                />
+                <div className="text-lg">
+                  <p className="text-gray-800 leading-none font-bold">
+                    Jonathan Reinink
+                  </p>
+                  <p className="text-gray-500">Available Cars</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="h-40 w-1/5 border rounded-lg overflow-hidden shadow-lg bg-white  m-10 px-4 py-2 flex justify-center">
-          <div className="flex items-center">
-            <img
-              className="w-16 h-16 rounded-full mr-4"
-              src={profileImage}
-              alt=""
-            />
-            <div className="text-lg">
-              <p className="text-gray-800 leading-none font-bold">
-                Jonathan Reinink
-              </p>
-              <p className="text-gray-500">Avg Treat. Costs</p>
-            </div>
-          </div>
-        </div>
-        <div className="h-40 w-1/5 border rounded-lg overflow-hidden shadow-lg bg-white  m-10 px-4 py-2 flex justify-center">
-          <div className="flex items-center">
-            <img
-              className="w-16 h-16 rounded-full mr-4"
-              src={profileImage}
-              alt=""
-            />
-            <div className="text-lg">
-              <p className="text-gray-800 leading-none font-bold">
-                Jonathan Reinink
-              </p>
-              <p className="text-gray-500">Available Cars</p>
-            </div>
-          </div>
+          
         </div>
       </div>
-    
-    )
-}
+    </div>
+  );
+};
--- src/container/App.js
+++ src/container/App.js
@@ -14,8 +14,16 @@ function App() {
         {/* navbar area */}
         <NavBar />
         {/* main area */}
-        <main className="flex flex-col">
-            <ResumeArea/>
+        <main className="flex flex-col container ml-20 mr-5">
+          <ResumeArea/>
+          <div className="flex flex-row justify-between">
+            <div className=" m-5 w-3/4 h-64 border rounded-lg overflow-hidden shadow-lg bg-white">
+                hh
+            </div>
+            <div className=" m-5 w-1/4 h-64 border rounded-lg overflow-hidden shadow-lg bg-white">
+                hh
+            </div>
+          </div>
         </main>
       </div>
     </div>
