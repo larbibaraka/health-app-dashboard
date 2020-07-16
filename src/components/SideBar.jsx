69b2d5f70b2f6cb977888636d4ab322eb8df9083
Baraka Larbi <larbibaraka@gmail.com>
2020-07-16 18:47:18 +0100

    create basic resume again !!

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,7 @@ export const SideBar = () => {
   return (
     <div className="sidebar flex flex-col bg-white w-1/5">
       <div className="logo h-16 justify-center ">SomeLogo</div>
-      <div className="bg-gray-200 menu p-6 flex flex-col">
+      <div className="bg-white menu p-6 flex flex-col">
         <div className="register-patient self-center">
           <button className="bg-indigo-700 text-gray-200 px-6 py-3 rounded-md font-semibold   hover:bg-indigo-800 hover:text-white items-center focus:outline-none">
             Register patient +
