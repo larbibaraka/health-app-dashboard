1df5bf9a62efe11b0c98dd1648b0f5143961fa3a
Baraka Larbi <larbibaraka@gmail.com>
2020-07-17 22:42:17 +0100

    add text under line

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -29,43 +29,43 @@ export const SideBar = () => {
         </div>
         <div className="menu-elements mt-5 flex flex-col">
           <ul className="flex flex-col ">
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center hover:underline ">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800 ">
                 <FontAwesomeIcon icon={faUserFriends} />
               </span>
               Patients
             </li>
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center hover:underline ">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800 ">
                 <FontAwesomeIcon icon={faChartPie} />
               </span>
               OverView
             </li>
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center hover:underline ">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800">
                 <FontAwesomeIcon icon={faMapMarkedAlt} />
               </span>
               Map
             </li>
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center hover:underline ">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800 ">
                 <FontAwesomeIcon icon={faHome} />
               </span>
               Departements
             </li>
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6 p-3  text-center  hover:underline">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800 ">
                 <FontAwesomeIcon icon={faUserNurse} />
               </span>
               Doctors
             </li>
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6  p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-6  p-3  text-center hover:underline ">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800 ">
                 <FontAwesomeIcon icon={faHistory} />
               </span>
               History
             </li>
-            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-12 p-3  text-center  ">
+            <li className="text-gray-700 font-sans font-bold hover:text-purple-800 cursor-pointer mt-12 p-3  text-center hover:underline ">
               <span className="w-12 h-12  mr-4 text-gray-600 cursor-pointer hover:text-purple-800 ">
                 <FontAwesomeIcon icon={faSlidersH} />
               </span>
